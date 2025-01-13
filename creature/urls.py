fb2ee24a33f3ac128bfba7e0ef6280d13254b1a3
Joanne Wright <esonlcmh@students.codeinstitute.net>
2025-01-13 19:10:31 +0000

    create templates

--- creature/urls.py
+++ creature/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.AnimalList.as_view(), name='home'),
+    path('<slug:slug>/', views.animal_detail, name='animal_detail'),
 ]
--- creature/views.py
+++ creature/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404, reverse
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Animal
 
@@ -8,3 +8,26 @@ class AnimalList(generic.ListView):
     queryset = Animal.objects.all()
     template_name = "animal/index.html"
     paginate_by = 6
+
+def animal_detail(request, slug):
+    """
+    Display an individual :model:`creature.Animal`.
+
+    **Context**
+
+    ``post``
+        An instance of :model:`blog.Post`.
+
+    **Template:**
+
+    :template:`blog/post_detail.html`
+    """
+
+    queryset = Animal.objects.all()
+    post = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "creature/animal_detail.html",
+        {"post": post},
+    )
