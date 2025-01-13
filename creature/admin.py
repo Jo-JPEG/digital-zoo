c723e8ec7786c7ddd484002e08ee17c8096fbe61
Joanne Wright <esonlcmh@students.codeinstitute.net>
2025-01-13 15:12:33 +0000

    add summernote

--- creature/admin.py
+++ creature/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from .models import Animal
+from django_summernote.admin import SummernoteModelAdmin
+
+@admin.register(Animal)
+class PostAdmin(SummernoteModelAdmin):
+
+    list_display = ('name', 'slug')
+    search_fields = ['name']
+    prepopulated_fields = {'slug': ('name',)}
+    summernote_fields = ('description',)
 
 # Register your models here.
-admin.site.register(Animal)
+# admin.site.register(Animal)
