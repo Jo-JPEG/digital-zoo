9fda410c5f45ddf4201d62814792ece8cbbbb474
Joanne Wright <esonlcmh@students.codeinstitute.net>
2025-01-13 18:14:16 +0000

    add templates

--- creature/urls.py
+++ creature/urls.py
@@ -1,6 +1,7 @@
-from . import views
 from django.urls import path
+from . import views
 
 urlpatterns = [
     path('', views.AnimalList.as_view(), name='home'),
+    path('<slug:slug>/', views.animal_detail, name='animal_detail'),
 ]
--- creature/views.py
+++ creature/views.py
@@ -1,8 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic
 from .models import Animal
 
 # Create your views here.
 
 class AnimalList(generic.ListView):
-    model = Animal
+    queryset = Animal.objects.all()
+    template_name = "animal/index.html"
+
+
+def animal_detail(request, slug):
+    queryset = Animal.objects.all()
+    post = get_object_or_404(queryset, slug=slug)
+
+    return render(request)
